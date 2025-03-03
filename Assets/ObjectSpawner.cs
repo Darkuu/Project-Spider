8584e16247099cfe2cd1d27bfccc482a8739d618
Darkuu <70687832+Darkuu@users.noreply.github.com>
2025-03-03 18:11:01 +0200

    Working on a spawner system
    fgff

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float cooldownTimer;
+        
+
+    void Update()
+    {
+        
+    }
+}
