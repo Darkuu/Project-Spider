db200ebb0a51346f3f9c2ed52e7460f329af2775
Darkuu <70687832+Darkuu@users.noreply.github.com>
2025-03-25 17:36:21 +0200

    Added a Fade for main Menu

--- Assets/Scripts/Main Menu/MainMenuButtons.cs
+++ Assets/Scripts/Main Menu/MainMenuButtons.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    public Animator transition;
+    [SerializeField] private float transitionTime;
 
-    public void LoadScene()
+    public void PlayGame()
     {
-        SceneManager.LoadScene("Gameplay");
+        StartCoroutine(LoadLevel());
     }
 
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene("Gameplay");
+    }
 }
