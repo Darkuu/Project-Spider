8d5af6f4de28ce475bad186b664dc1510c8c9e66
Darkuu <70687832+Darkuu@users.noreply.github.com>
2025-04-07 21:07:21 +0300

    Updated Main menu layout & added options

--- Assets/Scripts/Main Menu/MainMenuButtons.cs
+++ Assets/Scripts/Main Menu/MainMenuButtons.cs
@@ -6,6 +6,7 @@ public class MainMenuButtons : MonoBehaviour
 {
     public Animator transition;
     [SerializeField] private float transitionTime;
+    [SerializeField] private float fadeOutDuration = 1f; 
 
     public void PlayGame()
     {
@@ -14,8 +15,31 @@ public class MainMenuButtons : MonoBehaviour
 
     IEnumerator LoadLevel()
     {
+        if (AudioManager.instance != null)
+        {
+            StartCoroutine(FadeOutMusic(fadeOutDuration));
+        }
+
         transition.SetTrigger("Start");
+        yield return new WaitForSeconds(fadeOutDuration);
         yield return new WaitForSeconds(transitionTime);
+
         SceneManager.LoadScene("Gameplay");
+    }
+
+    // Method to fade out music volume gradually
+    private IEnumerator FadeOutMusic(float duration)
+    {
+        float startVolume = AudioManager.instance.musicVolume; 
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            AudioManager.instance.SetMusicVolume(Mathf.Lerp(startVolume, 0f, timeElapsed / duration)); 
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        AudioManager.instance.SetMusicVolume(0f); 
     }
 }
